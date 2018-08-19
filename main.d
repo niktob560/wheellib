9206b8e384234bdb320333c5591f2d010d41b956
niktob560 <niktob560@gmail.com>
2018-08-19 15:16:28 +0000

    cleanup

--- main.d
+++
@@ -1,5 +0,0 @@
-main.o: main.cpp avr-api/api.h avr-api/settings.h \
- avr-api/digitalRegisters.h avr-api/digitalRegisters.c \
- avr-api/delayMicroseconds.c avr-api/TimerModes_2560.h avr-api/Timers.h \
- avr-api/timerCustomfuncAddrs.h avr-api/Timers.c avr-api/USART.h \
- avr-api/stringFuncs.h avr-api/stringFuncs.c avr-api/USART.c
